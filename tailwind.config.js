ad61e365f38b52bf922851fafb37a31cb06b9f73
fwqaaq <fwqaaq@gmail.com>
2023-05-22 11:48:22 +0800

    feat: update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ import {
   createVariableColors,
   variableColorsPlugin,
 } from 'tailwindcss-variable-colors';
-import { addDynamicIconSelectors } from '@iconify/tailwind';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -18,7 +17,7 @@ const config = {
       },
     },
   },
-  plugins: [addDynamicIconSelectors(), variableColorsPlugin(defaultColors)],
+  plugins: [variableColorsPlugin(defaultColors)],
 };
 
 export default config;
